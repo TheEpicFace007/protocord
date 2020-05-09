19f7438a686f6eefa6ea748f8785f54508e898a1
Vini Dalvino <vinidalvino@cock.li>
2020-05-09 14:49:01 -0400

    Added  the image url. it's so borring to do

--- URL.lua
+++ URL.lua
@@ -1,3 +1,12 @@
-baseurl = "https://discordapp.com/api"
-url = {}
-return baseurl,url
+local baseurl = "https://discordapp.com/api"
+local imageBaseUrl = "https://cdn.discordapp.com/"
+local imageurl = {
+    customEmoji = imageBaseUrl .. "/emojis/";
+    guildIcon = imageBaseUrl .. "/icons/";
+    guildSplash = imageBaseUrl .. "/splash/";
+    guildDiscoverSplash = imageBaseUrl .. "/discovery-splashes";
+    guildBanner = imageBaseUrl .. "/banners/";
+    defaultUserAvatar = imageBaseUrl .. "/embed/avatars/";
+    userAvatar = imageBaseUrl .. "/avatars/";
+}
+return baseurl,imageurl
